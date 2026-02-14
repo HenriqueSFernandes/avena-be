4bee047dd69d3d6da5e3353afdcde5e14f570eeb
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-02-14 18:43:25 +0000

    feat: added ip to main

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,10 @@ const app = new Elysia()
 	.get("/protected", ({ user }) => user, {
 		auth: true,
 	})
-	.listen(3000);
+	.listen({
+		port: 3000,
+		hostname: "0.0.0.0",
+	});
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
